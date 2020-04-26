20c4be4c58e4af06332a81df265b64abb3831e9b
prince naija <princeachuru@gmail.com>
2020-04-26 18:47:39 -0400

    Password generator

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # myhw3
+This project was a password generator web application. It was designed using HTML, CSS and Javascript.
+The HTML and CSS external style file were both provided for us.
+I created a script javascript file and linked it to my provied html file.
+
+The first step was listing out the variables I would be using at the top of the script file and creating a varaiable for each of the possible charater types to be included in the password.
+
+Then I created the funtion that would prompt the user to specify the password character length. I then looped this prompt using the while loop so as long as the conditions I set (password_length < 8 || password_length > 128) were true the prompt would continue looping until the password length selected by the user was no less than 8 and no more than 128 characters.
+
+Next I set up another while loop this time for the different charcter types the user is prompted to confirm using the confirm function. The character types are set as booleans and given the value false listed at the top of the page. If the user doesnt choose to include any of the character types they are hit with an alert message specifying that atleast one of the chacrter types must be chosen and are looped back to the confirm messages until at least one of the types are confirmed to be included in the password. 
+
+Next I created the function for selecting a random element from an array and within the function I set up conditional if statments so that if the different character type booleans are true meaning the user has chosen to include it in their password the function concat will run joining each chacrter type string listed at the top of the page into an empty string variable labeled user input declared at the top of the page. 
+
+Next I created the generate password function and within the function created a for loop that updates the password variable, each time picking a random index in user Input and loops for the desired password Length chosen at the begining prompt, using the math random and math floor function to return the random inegers into the password variable. 
+
+Finally I created the write password function that assigns the password text element a value that is equal to the password variable updated in the previous step. I then list each of these functions within the add event listener function so when the generate button is pressed each of these lines of code will begin running in order until finally the password is generated in the password text input.  
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-// Array of special characters to be included in password
+// variables declared 
 var password_length;
-var ifspecialCharacters = true;
-var ifnumericCharacters = true;
-var iflowerCasedCharacters = true;
-var ifupperCasedCharacters = true;
-var characterArray = [];
-var passwordspecs = "password specifications"
+var ifspecialCharacters = false;
+var ifnumericCharacters = false;
+var iflowerCasedCharacters = false;
+var ifupperCasedCharacters = false;
+var password = "";
+var userinput = [];
+var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
+// Array of special characters to be included in password
 var specialCharacters = [
   "@",
   "%",
@@ -102,92 +105,52 @@ function yourFuncForUserInput() {
         alert("Password must be between 8 to 128 charcters");
         password_length = prompt("How many characters do you want your password to be?");
         }
+  while(ifspecialCharacters === false && ifnumericCharacters === false && iflowerCasedCharacters === false && ifupperCasedCharacters === false){
   // 3. Variable to store boolean regarding the inclusion of special characters
-       var specialCharacters = confirm("Click ok to include special characters");
+       ifspecialCharacters = confirm("Click ok to include special characters");
   // 4. Variable to store boolean regarding the inclusion of numeric characters
-       var numericCharacters = confirm("Click ok to include numeric characters");
+       ifnumericCharacters = confirm("Click ok to include numeric characters");
   // 5. Variable to store boolean regarding the inclusion of lowercase characters
-       var lowerCasedCharacters = confirm("Click ok to include lower case characters");
+       iflowerCasedCharacters = confirm("Click ok to include lower case characters");
   // 6. Variable to store boolean regarding the inclusion of uppercase characters
-        var upperCasedCharacters = confirm("Click ok to include upper case characters");
+        ifupperCasedCharacters = confirm("Click ok to include upper case characters");
   // 7. Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
-        if (specialCharacters === false & numericCharacters === false & lowerCasedCharacters === false & upperCasedCharacters === false);
-  // 8. Object to store user input
-        var userinput = [ifspecialCharacters, ifnumericCharacters, iflowerCasedCharacters, ifupperCasedCharacters];
-  // 9. return user-input-object
-        return userinput;
-}
-
-// Function for getting a random element from an array
+        if (ifspecialCharacters === false && ifnumericCharacters === false && iflowerCasedCharacters === false && ifupperCasedCharacters === false){
+            alert("Password must include at least one of these charcters");
+            }
+        }
+    }    
+ // Function for getting a random element from an array
 function yourFuncForRandomChar() {
-return (specialCharacters.length -1);
+    if(ifspecialCharacters){
+        userinput = userinput.concat(specialCharacters);
     }
-
-
-
-  // 1. Generate random number range from 0 to (arr.length - 1)
-  //
-  // 2. Variable to store the element in arr usiong random number as index
-  //
-  // 3. return random elment in arr
-  //
-
-
+    if(ifnumericCharacters){
+       userinput = userinput.concat(numericCharacters);
+    }
+    if(iflowerCasedCharacters){
+       userinput = userinput.concat(lowerCasedCharacters);
+    }
+    if(ifupperCasedCharacters){
+        userinput = userinput.concat(upperCasedCharacters);
+    } 
+}
 // Function to generate password with user input
 function generatePassword() {
-  //
-  // DATA STRUCTURE
-  //
-  // 1. Varialable to store user input object by calling yourFuncForUserInput
-  //
-  // 2. An empty array for result to store password as it's being concatenated later
-  //
-  // 3. Array to store all eligible/possible characters of different types based on by user input
-  //
-  // 4. Array to contain one of each type of chosen character to ensure each will be used and guaranteed
-  //
-  // MAIN LOGIC
-  //
-  // 1a. Conditional statement that adds array of special characters into array of possible characters based on user input
-  // 1b. Push new random special character to guaranteedCharacters
-  //
-  // 2a. Conditional statement that adds array of numeric characters into array of possible characters based on user input
-  // 2b. Push new random special character to guaranteedCharacters
-  //
-  // 3a. Conditional statement that adds array of lowercase characters into array of possible characters based on user input
-  // 3b. Push new random lower-cased character to guaranteedCharacters
-  //
-  // 4a. Conditional statement that adds array of uppercase characters into array of possible characters based on user input
-  // 4b. Push new random upper-cased character to guaranteedCharacters
-  //
-  // 5. For loop to iterate over the password length from the options object, selecting random indices from the array of possible characters and concatenating those characters into the result variable
-  //
-  // 6. Mix in at least one of each guaranteed character in the result
-  //
-  // 7. Transform the result into a string and pass into writePassword and return it to the caller
-  //
+ for(var i = 0; i < password_length; i++){
+   password += userinput[Math.floor(Math.random()*userinput.length)];
+    }
 }
-
-//
-// Call back function to write password to the #password iuput
+ // Call back function to write password to the #password iuput
 function writePassword() {
-  // 1. Call YourFuncToGeneratePwd
-  var password = generatePassword();
-
-  // 2. Grab #password html tag
-  var passwordText = document.querySelector("#password");
-
-  // 3. Update the value of the tag
-  passwordText.value = password;
+passwordText.value = password;
 }
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", function(event) {
   if (event.target.matches("button")){
     yourFuncForUserInput();
-  }
-
+    yourFuncForRandomChar();
+    generatePassword();
+    writePassword();
+}
 })
